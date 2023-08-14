b2d6ed06a6be28cf5ba65eeda38b8fd8b5521cf4
Christopher Haris <christoharis2005@gmail.com>
2023-08-14 16:53:49 +0700

    testing

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -67,7 +67,7 @@ const SignIn = () => {
     e.preventDefault();
     dispatch(loginStart());
     try {
-      const res = await axios.post("https://tokopedia-play-clone-backend-production.up.railway.app/api/auth/signin", { name, password });
+      const res = await axios.post("/api/auth/signin", { name, password });
       dispatch(loginSuccess(res.data));
     } catch (err) {
       dispatch(loginFailure());
